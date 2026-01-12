6875a9c22c260f30f28d990f3ea59213db6fefaf
Nabil Dellagi <nabildellagi@gmail.com>
2026-01-12 20:44:51 +0100

    Delete projectmain directory

--- projectmain/faculte/generate_hash.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Génère un vrai hash bcrypt pour le mot de passe "admin123"
-$password = "admin123";
-$hash = password_hash($password, PASSWORD_BCRYPT);
-
-echo "Copier ce hash:<br>";
-echo "<strong>" . $hash . "</strong><br><br>";
-echo "À mettre dans phpMyAdmin dans le champ mot_de_passe de l'utilisateur admin";
-?>
